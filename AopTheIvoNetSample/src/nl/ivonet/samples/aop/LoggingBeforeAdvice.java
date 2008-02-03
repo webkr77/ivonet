3bf3be69d43cc4e773b3b47e0302f30e53529e44
IvoNet <IvoNet@79b2f601-4845-0410-b3ec-2dd7504b846e>
2008-02-03 23:28:00 +0000

    Initial import.
    added AopTheIvoNetSample. a simple example of how aop works with the springframwork. This example uses no annotations but the applicationContext.xml to configure the pointcut ets.
    
    Just add the spring jars to your classpath and there dependencies and it should work just fine

---
+++ AopTheIvoNetSample/src/nl/ivonet/samples/aop/LoggingBeforeAdvice.java
@@ -0,0 +1,10 @@
+package nl.ivonet.samples.aop;
+
+
+public class LoggingBeforeAdvice {
+	public void log() {
+		System.out.println("=================================================");
+		System.out.println("calling " );
+	}
+
+}
